c6d546b8e226b5b1bdb9d174d3eb3b13a031b26c
Josir <josircg@gmail.com>
2019-04-26 12:26:20 -0300

    Crawler

--- core/management/commands/crawler.py
+++ core/management/commands/crawler.py
@@ -70,11 +70,11 @@ class Command(BaseCommand):
             status = 'A'
             tweepy_stream = tweepy.Stream(auth=api.auth, listener=listener)
             tweepy_stream.filter(track=[termo.busca], is_async=True)
-            while listener.checkpoint > 0 and listener.dtfinal < agora and status != 'P':
-                time.sleep(720)
+            while listener.checkpoint > 0 and listener.dtfinal > agora and status == 'P':
+                time.sleep(360)
                 agora = datetime.now(pytz.timezone(TIME_ZONE))
                 status = Termo.objects.get(id=termo.id).status
-                listener.checkpoint -= 1
+                listener.checkpoint -= 5
                 print('Checkpoint %d' % listener.checkpoint)
 
             # se saiu do loop pois ficou muito tempo sem encontrar tweets, mantem a busca ativa
@@ -101,7 +101,9 @@ class Command(BaseCommand):
                 if termo.dtfinal < agora:
                     Termo.objects.filter(id=termo.id).update(status='C')
                 else:
-                    Termo.objects.filter(id=termo.id).update(status='A')
+                    if status_proc != 'I':
+                        Termo.objects.filter(id=termo.id).update(status='A')
+
                 print('Processamento concluído')
             else:
                 print('Nenhum termo para processar')
