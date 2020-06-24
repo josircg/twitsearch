2296eae0884a8fac0fc7f0310eea63ec9a63aa40
josircg <josircg@gmail.com>
2020-06-24 15:45:41 -0300

    erro static

--- twitsearch/settings.py
+++ twitsearch/settings.py
@@ -118,10 +118,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-  os.path.join(MEDIA_ROOT),
-)
+
+#STATICFILES_DIRS = (
+#  os.path.join(MEDIA_ROOT),
+#)
 # django-admin-tools
+
 ADMIN_TOOLS_MENU = 'menu.CustomMenu'
 ADMIN_TOOLS_INDEX_DASHBOARD = 'dashboard.CustomIndexDashboard'
 ADMIN_TOOLS_APP_INDEX_DASHBOARD = 'dashboard.CustomAppIndexDashboard'
