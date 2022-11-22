311445c8a634594a262fa0d67f553abc236573ef
josircg <josircg@gmail.com>
2022-11-22 18:06:39 -0300

    Update User Data

--- core/management/commands/test3.py
+++ core/management/commands/test3.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import json
-import datetime
 import requests
 import pytz
 
+from datetime import datetime
 from django.core.management.base import BaseCommand
 from twitsearch.settings import TIME_ZONE
 from core.models import convert_date, TweetUser, FollowersHistory
@@ -11,69 +11,69 @@ from core.models import convert_date, TweetUser, FollowersHistory
 from searchtweets import load_credentials
 
 
-def save_result(data, processo):
-    data['process'] = processo
-    filename = 'data/%s_.json' % data['id']
-    arquivo = open(filename, 'w')
-    json.dump(data, arquivo)
-    arquivo.close()
-    print('%s saved' % filename)
-
-
 class Command(BaseCommand):
     label = 'Teste API v2 - User data'
+    agora = datetime.now(pytz.timezone(TIME_ZONE))
+    header = None
+    tot_registros = 0
+    tot_updates = 0
+
+    def update_users(self, user_list):
+        response = requests.get(
+            'https://api.twitter.com/2/users?ids=%s&user.fields=id,created_at,location,name,public_metrics,username,verified' %
+            ','.join(user_list),
+            headers=self.header)
+
+        # alterar o TweetUser com os dados obtidos
+        if response.status_code == 200:
+            user_data = response.json()['data']
+            for user_record in user_data:
+                user = TweetUser.objects.get(twit_id=user_record['id'])
+                user.name = user_record['name']
+                user.username = user_record['username']
+                user.verified = user_record['verified']
+                if 'location' in user_record:
+                    user.location = user_record['location']
+                user.created_at = convert_date(user_record['created_at']).date()
+                user.save()
+                self.tot_updates += 1
+
+                try:
+                    FollowersHistory.objects.get(user=user)
+                except FollowersHistory.DoesNotExist:
+                    follow = FollowersHistory(user=user, dt=self.agora,
+                                              followers=user_record['public_metrics']['followers_count'],
+                                              favourites=user_record['public_metrics']['following_count'])
+                    follow.save()
+
+                follow = FollowersHistory.objects.filter(user=user).latest('dt')
+                if user.followers != follow.followers:
+                    user.followers = follow.followers
+                    user.save()
 
     def handle(self, *args, **options):
         auth = load_credentials(filename="twitsearch/credentials.yaml",
                                 yaml_key="oldimar",
                                 env_overwrite=False)
 
-        header = {'Authorization': 'Bearer %s' % auth['bearer_token']}
+        self.header = {'Authorization': 'Bearer %s' % auth['bearer_token']}
 
-        tot_registros = 0
-        tot_updates = 0
-        agora = datetime.now(pytz.timezone(TIME_ZONE))
         user_list = []
         for empty_user in TweetUser.objects.filter(username__isnull=True).only('twit_id'):
 
             user_list.append(str(empty_user.twit_id))
-            tot_registros += 1
-            if tot_registros % 100 == 0:
-
-                response = requests.get(
-                    'https://api.twitter.com/2/users?ids=%s&user.fields=id,created_at,location,name,public_metrics,username,verified' %
-                    ','.join(user_list),
-                    headers=header)
-
-                # alterar o TweetUser com os dados obtidos
-                if response.status_code == 200:
-                    user_data = response.json()['data']
-                    for user in user_data:
-                        num_updates = \
-                            TweetUser.objects.filter(twit_id=user['id']).\
-                            update(location=user['location'], name=user['name'], username=user['username'],
-                                   verified=user['verified'], created_at=convert_date(user['created_at']).date())
-                        tot_updates += num_updates
-
-                        try:
-                            FollowersHistory.objects.get(user=user)
-                        except FollowersHistory.DoesNotExist:
-                            follow = FollowersHistory(user=user, dt=agora,
-                                                      followers=user['public_metrics']['followers_count'],
-                                                      favourites=user['public_metrics']['favourites_count'])
-                            follow.save()
-
-                        follow = FollowersHistory.objects.filter(user=user).latest('dt')
-                        if user.followers != follow.followers:
-                            user.followers = follow.followers
-                            user.save()
+            self.tot_registros += 1
+            if self.tot_registros % 100 == 0:
+                self.update_users(user_list)
                 user_list = []
 
-                if tot_registros % 2000:
+                if self.tot_registros % 2000 == 0:
                     break
 
-        print(f'Total de usuários lidos: {tot_registros}')
-        print(f'Total de usuários atualizados: {tot_updates}')
+        if len(user_list) > 0:
+            self.update_users(user_list)
+        print(f'Total de usuários lidos: {self.tot_registros}')
+        print(f'Total de usuários atualizados: {self.tot_updates}')
 
 
 
