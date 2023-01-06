2a0bfb711cec2110feabe8edf8b996acb38fa2a6
josircg <josircg@gmail.com>
2023-01-06 14:47:04 -0300

    Correção na geração do CSV

--- core/views.py
+++ core/views.py
@@ -82,7 +82,7 @@ def stats(request, id):
                 writer.writerow(rec)
                 alcance += max(int(rec[1]), int(rec[2]))
         csvfile.close()
-        projeto.alcance = max(alcance, int(projeto.tot_retwits))
+        projeto.alcance = max(alcance, int(projeto.tot_retwits.replace('.','')))
         projeto.save()
 
     dataset = []
