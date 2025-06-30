6ddead2215b66757e2b310b4dfca9a689b056fe5
Bonfim Justino do Nascimento <47996966+bonfimjustino7@users.noreply.github.com>
2025-06-30 13:41:33 -0300

    feat: opensearch
    * feat: salva twitter no opensearch
    
    * Refactor
    
    * feat: ajuste ao instânciar a classe Crawler
    
    * feat: ajustes
    
    * refac: remove imports não usados
    
    ---------
    
    Co-authored-by: josircg <josircg@gmail.com>

--- core/management/commands/crawler.py
+++ core/management/commands/crawler.py
@@ -96,7 +96,7 @@ def busca_stream(termo, listener):
 
 class Crawler:
 
-    def __init__(self, limite=2000):
+    def __init__(self, limite=2000, index_name=None):
         self.since_id = None
         self.until_id = None
         self.tot_registros = 0
@@ -104,8 +104,8 @@ class Crawler:
         self.ultimo_tweet = 0
         self.dt_inicial = None
         self.client = connect_opensearch('minerva-teste')
-        if self.client:
-            create_if_not_exists_index(self.client)
+        if self.client and index_name:
+            create_if_not_exists_index(self.client, index_name)
 
 
     def search_recent(self, processo):
@@ -145,9 +145,21 @@ class Crawler:
                          query=termo.busca,
                          tweet_fields='text,created_at,public_metrics,author_id,conversation_id,lang,'
                                       'referenced_tweets,attachments,geo',
-                         user_fields=['username', 'public_metrics', 'created_at', 'location'],
-                         expansions=['author_id','referenced_tweets.id',
-                                     'referenced_tweets.id.author_id'],
+                         user_fields=['username', 'public_metrics', 'created_at', 'location'],                        
+                         expansions=[
+                            'article.cover_media',
+                            'article.media_entities',
+                            'attachments.media_keys',
+                            'attachments.media_source_tweet',
+                            'author_id',
+                            'entities.mentions.username',
+                            'geo.place_id',
+                            'in_reply_to_user_id',
+                            'entities.note.mentions.username',
+                            'referenced_tweets.id',
+                            'referenced_tweets.id.attachments.media_keys',
+                            'referenced_tweets.id.author_id'
+                        ],
                          next_token=next_token,
                          since_id=self.since_id,
                          until_id=self.until_id,
@@ -249,8 +261,9 @@ def processa_termo(termo, limite):
                                             tipo=termo.tipo_busca, status=Processamento.PROCESSANDO)
     Termo.objects.filter(id=termo.id).update(status='P')
     commit()
-
-    crawler = Crawler(limite)
+    
+    index_name = f"twitter-{agora.year}-{agora.month}"
+    crawler = Crawler(limite, index_name=index_name)
     try:
         crawler.search_recent(processo)
         processo.twit_id = crawler.ultimo_tweet
---
+++ core/management/commands/twitter_test.py
@@ -0,0 +1,40 @@
+import json
+import requests
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = 'Recupera um tweet específico pelo ID'
+
+    def add_arguments(self, parser):
+        parser.add_argument('twit_id', type=str, help='ID do Tweet')
+
+    def handle(self, *args, **options):
+        twit_id = options['twit_id']
+        
+        url = f"https://api.twitter.com/2/tweets/{twit_id}"
+
+        # Somente campos válidos permitidos
+        queryparams = {
+            "tweet.fields": "text,created_at,public_metrics,author_id,conversation_id,lang,referenced_tweets,attachments,geo",
+            "expansions": "article.cover_media,article.media_entities,attachments.media_keys,attachments.media_source_tweet,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,entities.note.mentions.username,referenced_tweets.id,referenced_tweets.id.attachments.media_keys,referenced_tweets.id.author_id",
+            "user.fields": "username,name,public_metrics,created_at,location",
+            
+        }
+
+        headers = {
+            "Authorization": f"Bearer {settings.BEARED_TOKEN}"
+        }
+
+        response = requests.get(url, headers=headers, params=queryparams)
+        data = response.json()
+
+        # Corrige erro de acesso a 'response.data'
+        filename = '%s/data/%s.json' % (settings.BASE_DIR,twit_id)
+        with open(filename, 'w') as arquivo:
+            json.dump(data, arquivo)        
+            
+
+        self.stdout.write(self.style.SUCCESS(
+            f'Comando de teste do Twitter executado com sucesso! twit_id: {twit_id}'))
