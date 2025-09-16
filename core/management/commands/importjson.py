7510b62ebf3f9449a62bda4715031adbd2d42dfb
josircg <josircg@gmail.com>
2025-09-15 22:01:11 -0300

    Test import

--- core/management/commands/importjson.py
+++ core/management/commands/importjson.py
@@ -358,8 +358,6 @@ class Command(BaseCommand):
                                 print('Mais de 10 erros encontrados')
                                 break
                         tot_files += 1
-                        if tot_files == 100:
-                            break
             finally:
                 if tot_files == 0:
                     if processo_ativo.tot_registros == 0:
