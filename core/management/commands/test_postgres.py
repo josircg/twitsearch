aa3c563e76c8473ff58fd8f99e998f70f85eec35
Josir <josircg@gmail.com>
2026-05-26 10:04:04 -0300

    Testa PG

---
+++ core/management/commands/test_postgres.py
@@ -0,0 +1,34 @@
+import psycopg2 as psycopg
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+def connect_postgresql(server: str):
+    server = settings.PG_SERVERS.get(server)
+    if not server:
+        raise Exception(f'Entrada "{server}" não encontrada')
+    host = server['host']
+    port = server.get('port', 5432)
+    database = server['database']
+    username = server['username']
+    password = server['password']
+    pg = psycopg.connect(f"dbname={database} user={username} password={password} host={host} port={port}")
+    return pg
+
+class Command(BaseCommand):
+    label = 'Teste PostgreSQL'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--server', type=str, help='Server')
+
+    def handle(self, *args, **options):
+        pg = connect_postgresql(options.get('server'))
+        if pg:
+            with pg.cursor() as cursor:
+                cursor.execute("SELECT version();")
+                record = cursor.fetchone()
+                print(f"Connected: {record[0]}")
+
+
+
