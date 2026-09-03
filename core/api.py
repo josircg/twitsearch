4dba48831ba34cf54b7bc08f71a6211e9749a77e
Josir <josircg@gmail.com>
2026-09-03 15:01:04 -0300

    Fix API :(

--- core/api.py
+++ core/api.py
@@ -52,7 +52,7 @@ def termos(request, rede_id):
     lista = []
     for termo in Termo.objects.filter(projeto__redes=rede_id).exclude(projeto__status='C').order_by('projeto'):
         # se for telegram, gerar a lista de canais
-        if rede_id == 4
+        if rede_id == 4:
             if termo.projeto.lista_canais:
                 canais = list(termo.projeto.lista_canais.canais.filter(
                     id_numerico__isnull=False, status='A').values_list('username', flat=True))
